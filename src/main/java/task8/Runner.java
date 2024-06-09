3901c4d52bdb7b9d2e1ad0da79d54a6f5d3a16ca
Gloff228 <romo4ka101@gmail.com>
2024-06-09 23:01:34 +0300

    Task 8. Коммит из ветки dev

--- src/main/java/task8/Runner.java
+++ src/main/java/task8/Runner.java
@@ -4,5 +4,6 @@ public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Коммит для ветвления");
+        System.out.println("Коммит из ветки dev");
     }
 }
