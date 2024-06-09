62f9070eab3ba1217d4e7461fec09c6d92558389
Gloff228 <romo4ka101@gmail.com>
2024-06-09 23:00:20 +0300

    Task 8. Создание коммита из которого будет происходить ветвление

--- src/main/java/task8/Runner.java
+++ src/main/java/task8/Runner.java
@@ -4,7 +4,5 @@ public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Коммит для ветвления");
-        System.out.println("Коммит из ветки dev");
-        System.out.println("Коммит из ветки master");
     }
 }
